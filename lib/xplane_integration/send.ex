c4f2fcca37a06ad151a889b10ee01036a0566a26
Greg Gradwell <greghgradwell@live.com>
2021-11-02 06:13:53 -0700

    Using UBX messages for all commands. Can receive variable-length direct actuator output.

--- lib/xplane_integration/send.ex
+++ lib/xplane_integration/send.ex
@@ -4,15 +4,12 @@ defmodule XplaneIntegration.Send do
   require Logger
   require ViaUtils.Shared.Groups, as: Groups
   require ViaUtils.Ubx.ClassDefs, as: ClassDefs
-  require ViaUtils.Ubx.AccelGyro.DtAccelGyro, as: DtAccelGyro
-  require ViaUtils.Ubx.VehicleCmds.BodyrateThrustCmd, as: BodyrateThrustCmd
   require ViaUtils.Ubx.VehicleCmds.ActuatorCmdDirect, as: ActuatorCmdDirect
   require ViaUtils.Ubx.VehicleCmds.BodyrateActuatorOutput, as: BodyrateActuatorOutput
   require ViaUtils.Shared.ActuatorNames, as: Act
   @cmd_header <<68, 65, 84, 65, 0>>
   @zeros_1 <<0, 0, 0, 0>>
   @zeros_3 <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0>>
-  # @zeros_4 <<0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0>>
   @zeros_5 <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0>>
   @zeros_7 <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0>>
 
@@ -38,7 +35,7 @@ defmodule XplaneIntegration.Send do
 
     state = %{
       socket: socket,
-      destination_ip_address: nil,
+      destination_ip: nil,
       destination_port: Keyword.fetch!(config, :destination_port),
       ubx: UbxInterpreter.new(),
       bodyaccel: %{},
@@ -49,11 +46,11 @@ defmodule XplaneIntegration.Send do
       agl: 0,
       airspeed: 0,
       new_simulation_data_to_publish: false,
-      request_ip_address_timer: request_ip_address_timer
+      request_ip_address_timer: request_ip_address_timer,
+      channel_names: Keyword.get(config, :channel_names, %{})
     }
 
     ViaUtils.Comms.Supervisor.start_operator(__MODULE__)
-    ViaUtils.Comms.join_group(__MODULE__, Groups.simulation_update_actuators())
     ViaUtils.Comms.join_group(__MODULE__, Groups.virtual_uart_actuator_output())
 
     {:ok, state}
@@ -62,7 +59,7 @@ defmodule XplaneIntegration.Send do
   @impl GenServer
   def handle_info(@request_ip_address_loop, state) do
     state =
-      if is_nil(state.destination_ip_address) do
+      if is_nil(state.destination_ip) do
         GenServer.cast(XplaneIntegration.Receive, {:get_ip_address, self()})
         state
       else
@@ -75,13 +72,14 @@ defmodule XplaneIntegration.Send do
 
   @impl GenServer
   def handle_info({:circuits_uart, _port, data}, state) do
-    Logger.debug("xpi rx: #{data}")
+    # Logger.debug("xpi rx: #{data}")
+    state = check_for_new_messages_and_process(:binary.bin_to_list(data), state)
     {:noreply, state}
   end
 
   @impl GenServer
   def handle_cast({:set_ip_address, ip_address}, state) do
-    {:noreply, %{state | destination_ip_address: ip_address}}
+    {:noreply, %{state | destination_ip: ip_address}}
   end
 
   @spec check_for_new_messages_and_process(list(), map()) :: map()
@@ -94,27 +92,49 @@ defmodule XplaneIntegration.Send do
     else
       %{msg_class: msg_class, msg_id: msg_id} = ubx
       # Logger.debug("msg class/id: #{msg_class}/#{msg_id}")
-      state =
-        case msg_class do
-          ClassDefs.vehicle_cmds() ->
-            case msg_id do
-              BodyrateActuatorOutput.id() ->
-                cmds =
-                  UbxInterpreter.deconstruct_message_to_map(
-                    BodyrateActuatorOutput.bytes(),
-                    BodyrateActuatorOutput.multipliers(),
-                    BodyrateActuatorOutput.keys(),
-                    payload
-                  )
+      case msg_class do
+        ClassDefs.vehicle_cmds() ->
+          case msg_id do
+            BodyrateActuatorOutput.id() ->
+              cmds =
+                UbxInterpreter.deconstruct_message_to_map(
+                  BodyrateActuatorOutput.bytes(),
+                  BodyrateActuatorOutput.multipliers(),
+                  BodyrateActuatorOutput.keys(),
+                  payload
+                )
 
+              %{
+                Act.aileron() => aileron_scaled,
+                Act.elevator() => elevator_scaled,
+                Act.throttle() => throttle_scaled,
+                Act.rudder() => rudder_scaled
+              } = cmds
+
+              throttle_scaled = ViaUtils.Math.get_one_sided_from_two_sided(throttle_scaled)
+
+              send_ail_elev_rud_commands(
+                aileron_scaled,
+                elevator_scaled,
+                rudder_scaled,
+                socket,
+                dest_ip,
+                dest_port
+              )
+
+              send_throttle_command(throttle_scaled, socket, dest_ip, dest_port)
+
+            ActuatorCmdDirect.id() ->
+              cmds = ActuatorCmdDirect.Utils.get_actuator_output(payload, state.channel_names)
+              # Logger.debug("direct act: #{inspect(cmds)}")
+              ail_elev_rud_cmds = Map.take(cmds, [Act.aileron(), Act.elevator(), Act.rudder()])
+
+              if map_size(ail_elev_rud_cmds) == 3 do
                 %{
                   Act.aileron() => aileron_scaled,
                   Act.elevator() => elevator_scaled,
-                  Act.throttle() => throttle_scaled,
                   Act.rudder() => rudder_scaled
-                } = cmds
-
-                throttle_scaled = ViaUtils.Math.get_one_sided_from_two_sided(throttle_scaled)
+                } = ail_elev_rud_cmds
 
                 send_ail_elev_rud_commands(
                   aileron_scaled,
@@ -124,106 +144,32 @@ defmodule XplaneIntegration.Send do
                   dest_ip,
                   dest_port
                 )
+              end
 
+              throttle_scaled = Map.get(cmds, Act.throttle())
+
+              unless is_nil(throttle_scaled) do
+                throttle_scaled = ViaUtils.Math.get_one_sided_from_two_sided(throttle_scaled)
                 send_throttle_command(throttle_scaled, socket, dest_ip, dest_port)
+              end
 
-                # Logger.debug("dt/accel/gyro values: #{inspect([dt, ax, ay, az, gx, gy, gz])}")
-                # Logger.debug("send dt/accel/gyro values: #{ViaUtils.Format.eftb_map(values, 3)}")
+              flaps_scaled = Map.get(cmds, Act.flaps())
 
-                %{state | ubx: UbxInterpreter.clear(ubx)}
+              unless is_nil(flaps_scaled) do
+                flaps_scaled = ViaUtils.Math.get_one_sided_from_two_sided(flaps_scaled)
+                send_flaps_command(flaps_scaled, socket, dest_ip, dest_port)
+              end
 
-            ActuatorCmdDirect.id ->
-cmds =
-                  UbxInterpreter.deconstruct_message_to_map(
-                    ActuatorCmdDirect.bytes(),
-                    ActuatorCmdDirect.multipliers(),
-                    ActuatorCmdDirect.keys(),
-                    payload
-                  )
-
-                %{
-                  Act.aileron() => aileron_scaled,
-                  Act.elevator() => elevator_scaled,
-                  Act.throttle() => throttle_scaled,
-                  Act.rudder() => rudder_scaled
-                } = cmds
-
-              _other ->
-                Logger.warn("Bad message id: #{msg_id}")
-                state
-            end
-
-          ViaUtils.Ubx.ClassDefs.vehicle_cmds() ->
-            case msg_id do
-              BodyrateThrustCmd.id() ->
-                TestHelper.Companion.Utils.display_bodyrate_thrust_cmd(payload)
-            end
-
-            %{state | ubx: UbxInterpreter.clear(ubx)}
-
-          _other ->
-            Logger.warn("Bad message class: #{msg_class}")
-            state
-        end
-
-      check_for_new_messages_and_process([], state)
-    end
-  end
-
-  def get_cmds_from_ubx_message(msg_class, msg_id, payload) do
-  end
-
-  def send_cmds(cmds, socket, dest_ip, dest_port) do
-    # Logger.debug("xp send rx up_act: #{ViaUtils.Format.eftb_map(actuators_and_outputs, 3)}")
-
-    dest_ip = state.destination_ip_address
-
-    unless is_nil(dest_ip) do
-      cmds =
-        Enum.reduce(actuators_and_outputs, %{}, fn {actuator_name, output}, acc ->
-          case actuator_name do
-            :flaps_scaled ->
-              Map.put(acc, actuator_name, ViaUtils.Math.get_one_sided_from_two_sided(output))
-
-            :gear_scaled ->
-              Map.put(acc, actuator_name, ViaUtils.Math.get_one_sided_from_two_sided(output))
-
-            :throttle_scaled ->
-              Map.put(acc, actuator_name, ViaUtils.Math.get_one_sided_from_two_sided(output))
-
-            name ->
-              Map.put(acc, name, output)
+            _other ->
+              Logger.warn("Bad message id: #{msg_id}")
           end
-        end)
 
-      socket = state.socket
-      dest_port = state.destination_port
-      send_ail_elev_rud_commands(cmds, socket, dest_ip, dest_port)
-      send_throttle_command(cmds, socket, dest_ip, dest_port)
-      send_flaps_command(cmds, socket, dest_ip, dest_port)
-    end
-
-    # Logger.debug("up act cmds: #{ViaUtils.Format.eftb_map(cmds, 3)}")
-    {:noreply, state}
-  end
-
-  @impl GenServer
-  def handle_cast({:send_commands, command_type, commands}, state) do
-    function =
-      case command_type do
-        :ail_elev_rud -> :send_ail_elev_rud_commands
-        :throttle -> :send_throttle_command
-        :flaps -> :send_flaps_command
+        _other ->
+          Logger.warn("Bad message class: #{msg_class}")
       end
 
-    apply(__MODULE__, function, [
-      commands,
-      state.socket,
-      state.destination_ip_address,
-      state.destination_port
-    ])
-
-    {:noreply, state}
+      check_for_new_messages_and_process([], %{state | ubx: UbxInterpreter.clear(ubx)})
+    end
   end
 
   @spec send_ail_elev_rud_commands(number(), number(), number(), any(), tuple(), integer()) ::
@@ -243,8 +189,9 @@ cmds =
       |> Kernel.<>(ViaUtils.Math.uint_from_fp(rudder_scaled, 32))
       |> Kernel.<>(@zeros_5)
 
-    # Logger.debug("ail/elev/rud: #{Map.get(commands, :aileron)}/#{Map.get(commands, :elevator)}/#{Map.get(commands, :rudder)}")
-    # Logger.debug("ail-elev-rud: #{ViaUtils.Format.eftb_map(commands, 3)}")
+    # Logger.debug(
+    #   "ail/elev/rud: #{ViaUtils.Format.eftb(aileron_scaled, 3)}/#{ViaUtils.Format.eftb(elevator_scaled, 3)}/#{ViaUtils.Format.eftb(rudder_scaled, 3)}"
+    # )
     :gen_udp.send(socket, dest_ip, port, buffer)
   end
 
@@ -255,15 +202,12 @@ cmds =
       |> Kernel.<>(ViaUtils.Math.uint_from_fp(throttle_scaled, 32))
       |> Kernel.<>(@zeros_7)
 
-    # |> Kernel.<>(<<0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0>>)
-    # Logger.debug("buffer: #{buffer}")
-    # Logger.debug("thr: #{Map.get(commands, :throttle_scaled)}")
     :gen_udp.send(socket, dest_ip, port, buffer)
   end
 
   @spec send_flaps_command(number(), any(), tuple(), integer()) :: atom()
   def send_flaps_command(flaps_scaled, socket, dest_ip, port) do
-    # Logger.error("flaps: #{Map.get(commands, :flaps_scaled)}")
+    # Logger.error("send flaps: #{flaps_scaled}")
     buffer =
       (@cmd_header <> <<13, 0, 0, 0>>)
       |> Kernel.<>(@zeros_3)
@@ -275,33 +219,4 @@ cmds =
 
     :gen_udp.send(socket, dest_ip, port, buffer)
   end
-
-  @spec send_ail_elev_rud_commands_test(map()) :: atom()
-  def send_ail_elev_rud_commands_test(commands) do
-    %{
-      Act.aileron() => aileron_scaled,
-      Act.elevator() => elevator_scaled,
-      Act.rudder() => rudder_scaled
-    } = commands
-
-    GenServer.cast(
-      __MODULE__,
-      {:send_commands, :ail_elev_rud, aileron_scaled, elevator_scaled, rudder_scaled}
-    )
-  end
-
-  @spec send_throttle_command_test(number()) :: atom()
-  def send_throttle_command_test(throttle_scaled) do
-    GenServer.cast(__MODULE__, {:send_commands, :throttle, throttle_scaled})
-  end
-
-  @spec send_flaps_command_test(number()) :: atom()
-  def send_flaps_command_test(flaps_scaled) do
-    GenServer.cast(__MODULE__, {:send_commands, :flaps, flaps_scaled})
-  end
-
-  # @spec get_one_sided_value(number()) :: number()
-  # def get_one_sided_value(two_sided_value) do
-  #   0.5 * two_sided_value + 0.5
-  # end
 end
